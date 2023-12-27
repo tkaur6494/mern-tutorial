1e9adce837f822561b521120222053ec67a267a4
tkaur6494 <tripat.kaur6@gmail.com>
2023-12-27 14:12:50 +0100

    Add editUser API

--- frontend/src/features/users/usersApi.js
+++ frontend/src/features/users/usersApi.js
@@ -4,7 +4,19 @@ export const getUserList = async () => {
   return api
     .get("users")
     .then((response) => {
-        return response?.data
+      return response?.data;
     })
     .catch((err) => console.log(err));
+};
+
+export const updateUser = async (updatedUser) => {
+  return api
+    .patch("users", updatedUser)
+    .then((response) => {
+      return {status:response.status, message: response.data.message};
+    })
+    .catch((err) => {
+      return {status:err.response.status, message: err.response.data.message}
+      
+    });
 };
